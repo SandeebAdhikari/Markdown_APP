f2d985e631a02ccab560248cfece29eae5fa1a6a
Sandeeb Adhikari <sa2734@njit.edu>
2024-11-15 14:50:17 -0500

    nav bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,22 @@
 import IconMenu from "/assets/icon-menu.svg";
+import IconMarkDown from "/assets/MARKDOWN.svg";
+import IconDoc from "/assets/icon-document.svg";
 
 const NavBar = () => {
   return (
-    <div className="flex items-center justify-center w-full h-[72px] ">
-      <img src={IconMenu} alt="Menu Icon" className="w-18 h-18 mr-[24.75px]" />
+    <div className="flex items-center -full h-[72px] bg-800">
+      <div className="bg-700 w-18 h-18 flex items-center justify-center py-[27px] px-[21px]">
+        <img src={IconMenu} alt="Menu Icon" className="w-[30px] h-[18px]" />
+      </div>
+      <img src={IconMarkDown} alt="Markdown Icon" className="ml-[24.75px]" />
+      <div className="ml-[30.15px] w-[1px] h-[40px] bg-600" />
+      <div className="ml-6 flex items-center justify-between gap-4">
+        <img src={IconDoc} alt="Document Icon" className="w-[18px] h-[18px]" />
+        <div className="flex  flex-col   w-full">
+          <p className="text-[13px] font-light text-500 ">Document Name</p>
+          <p className="text-[15px] font-regular text-100">welcome.md</p>
+        </div>
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,39 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+        "roboto-slab": ["Roboto Slab", "serif"],
+        "roboto-mono": ["Roboto Mono", "monospace"],
+      },
+      fontSize: {
+        h1: ["32px", { fontFamily: "roboto-slab", fontWeight: "bold" }],
+        h2: ["28px", { fontFamily: "roboto-slab", fontWeight: "300" }],
+        h3: ["24px", { fontFamily: "roboto-slab", fontWeight: "bold" }],
+        h4: ["20px", { fontFamily: "roboto-slab", fontWeight: "bold" }],
+        h5: ["16px", { fontFamily: "roboto-slab", fontWeight: "bold" }],
+        h6: ["14px", { fontFamily: "roboto-slab", fontWeight: "bold" }],
+        "body-s": [
+          "14px",
+          { fontFamily: "roboto", fontWeight: "400", letterSpacing: "0.125em" },
+        ],
+        "body-m": ["15px", { fontFamily: "roboto", fontWeight: "400" }],
+        paragraph: [
+          "13px",
+          { fontFamily: "roboto", fontWeight: "300", lineHeight: "24px" },
+        ],
+      },
+      lineHeight: {
+        tight: "1.2",
+        normal: "1.5",
+        loose: "2",
+        custom: "24px",
+      },
+      letterSpacing: {
+        wide: "0.125em",
+        wider: "0.2em",
+      },
+
       colors: {
         1000: "#151619",
         900: "#1D1F22",
