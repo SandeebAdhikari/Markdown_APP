bf691e994efcfe30c5569865212232d75a3e3d3f
Sandeeb Adhikari <sa2734@njit.edu>
2024-11-15 20:06:40 -0500

    markdown setup

--- src/components/MarkdownEditor.tsx
+++ src/components/MarkdownEditor.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import ReactMarkdown from "react-markdown";
 import { welcome } from "../utils/welcome";
+import IconPreview from "/assets/icon-show-preview.svg";
 
 const MarkdownEditor: React.FC = () => {
   const [markdownText, setMarkdownText] = useState<string>(welcome);
@@ -10,21 +11,32 @@ const MarkdownEditor: React.FC = () => {
   };
 
   return (
-    <div className="flex flex-col md:flex-row gap-8 p-8 min-h-screen bg-gray-900 text-white">
+    <div className="flex  w-full min-h-screen bg-300">
       {/* Markdown Editor */}
-      <div className="w-full md:w-1/2">
-        <h2 className="text-xl font-bold mb-4">Markdown Input</h2>
+      <div className="w-1/2 h-full bg-200">
+        <h2 className="text-[14px] text-500 letterSpacing-wider font-medium py-3 ml-4 ">
+          MARKDOWN
+        </h2>
         <textarea
           value={markdownText}
           onChange={handleChange}
-          className="w-full h-[70vh] p-4 bg-gray-800 text-white rounded-lg focus:outline-none resize-none"
+          className="w-full h-screen p-4 bg-100 text-black hide-scrollbar focus:outline-none resize-none"
         />
       </div>
-
+      <div className=" flex w-[1px] h-full" />
       {/* Markdown Preview */}
-      <div className="w-full md:w-1/2">
-        <h2 className="text-xl font-bold mb-4">Preview</h2>
-        <div className="prose prose-invert max-w-full bg-gray-800 p-4 rounded-lg">
+      <div className="w-1/2 flex flex-col items-center justify-center bg-200">
+        <div className="flex items-center justify-between w-full">
+          <h2 className="text-[14px] text-500 letterSpacing-wider font-medium py-3 ml-4">
+            PREVIEW
+          </h2>
+          <img
+            src={IconPreview}
+            alt="Preview Icon"
+            className="w-4 h-[11.2px] mr-4"
+          />
+        </div>
+        <div className="prose  max-w-full bg-100 p-4">
           <ReactMarkdown>{markdownText}</ReactMarkdown>
         </div>
       </div>
