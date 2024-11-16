747be3c64e09947d614e144fbce43d29e5cc5d9c
Sandeeb Adhikari <sa2734@njit.edu>
2024-11-15 21:31:27 -0500

    front-end complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import MarkdownEditor from "./components/MarkdownEditor";
+import MarkdownEditor from "./pages/MarkdownEditor";
 import NavBar from "./components/NavBar";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
+    <div className="min-h-screen w-full">
       <NavBar />
       <MarkdownEditor />
     </div>
--- src/pages/MarkdownEditor.tsx
+++ src/pages/MarkdownEditor.tsx
@@ -12,18 +12,18 @@ const MarkdownEditor: React.FC = () => {
 
   return (
     <div className="flex w-full min-h-screen bg-300">
-      <div className="w-1/2 h-full bg-200">
+      <div className="w-1/2 bg-200 h-screen flex flex-col">
         <h2 className="text-[14px] text-500 letterSpacing-wider font-medium py-3 ml-4">
           MARKDOWN
         </h2>
         <textarea
           value={markdownText}
           onChange={handleChange}
-          className="w-full h-screen p-4 bg-100 text-black hide-scrollbar focus:outline-none resize-none"
+          className="w-full flex-1 p-4 bg-100 text-black hide-scrollbar focus:outline-none resize-none"
         />
       </div>
-      <div className="flex w-[1px] h-full" />
-      <div className="w-1/2 flex flex-col items-center justify-center bg-200">
+      <div className="flex w-[1px] h-screen" />
+      <div className="w-1/2 bg-200 h-screen flex flex-col items-center justify-center">
         <div className="flex items-center justify-between w-full">
           <h2 className="text-[14px] text-500 letterSpacing-wider font-medium py-3 ml-4">
             PREVIEW
@@ -34,7 +34,7 @@ const MarkdownEditor: React.FC = () => {
             className="w-4 h-[11.2px] mr-4 hover:cursor-pointer"
           />
         </div>
-        <div className="prose max-w-full bg-100 p-4">
+        <div className="prose max-w-full bg-100 p-4 flex-1 overflow-auto hide-scrollbar">
           <ReactMarkdown>{markdownText}</ReactMarkdown>
         </div>
       </div>
