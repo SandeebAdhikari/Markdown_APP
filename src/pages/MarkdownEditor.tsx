4e6483c89fcbe35f14757f592975f42c206049e2
Sandeeb Adhikari <sa2734@njit.edu>
2024-11-16 10:39:20 -0500

    preview button

--- src/pages/MarkdownEditor.tsx
+++ src/pages/MarkdownEditor.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import ReactMarkdown from "react-markdown";
 import { useLocation, useParams } from "react-router-dom";
 import IconPreview from "/assets/icon-show-preview.svg";
+import IconHidePreview from "/assets/icon-hide-preview.svg";
 import { welcome } from "../utils/welcome";
 
 interface Document {
@@ -17,6 +18,7 @@ interface MarkdownEditorProps {
 const MarkdownEditor: React.FC<MarkdownEditorProps> = ({ documents }) => {
   const { docId } = useParams();
   const location = useLocation();
+  const [showPreviewOnly, setShowPreviewOnly] = useState(false);
 
   const isWelcomePage = location.pathname === "/";
   const currentDoc = documents.find((doc) => doc.name === `doc${docId}`);
@@ -37,30 +39,41 @@ const MarkdownEditor: React.FC<MarkdownEditorProps> = ({ documents }) => {
     setMarkdownText(event.target.value);
   };
 
+  const togglePreview = () => {
+    setShowPreviewOnly(!showPreviewOnly);
+  };
+
   return (
     <div className="flex w-full min-h-screen bg-300">
-      <div className="w-1/2 bg-200 h-screen flex flex-col">
-        <h2 className="text-[14px] text-500 letterSpacing-wider font-medium py-3 ml-4">
-          MARKDOWN
-        </h2>
-        <textarea
-          id="markdown-textarea"
-          value={markdownText}
-          onChange={handleChange}
-          className="w-full flex-1 p-4 bg-100 text-black hide-scrollbar focus:outline-none resize-none"
-        />
-      </div>
+      {!showPreviewOnly && (
+        <div className="w-1/2 bg-200 h-screen flex flex-col">
+          <h2 className="text-[14px] text-500 letterSpacing-wider font-medium py-3 ml-4">
+            MARKDOWN
+          </h2>
+          <textarea
+            id="markdown-textarea"
+            value={markdownText}
+            onChange={handleChange}
+            className="w-full flex-1 p-4 bg-100 text-black hide-scrollbar focus:outline-none resize-none"
+          />
+        </div>
+      )}
 
-      <div className="flex w-[1px] h-screen" />
+      {!showPreviewOnly && <div className="flex w-[1px] h-screen" />}
 
-      <div className="w-1/2 bg-200 h-screen flex flex-col items-center justify-center">
+      <div
+        className={`${
+          showPreviewOnly ? "w-full mx-[384px] " : "w-1/2"
+        } bg-200 h-screen flex flex-col items-center justify-center`}
+      >
         <div className="flex items-center justify-between w-full">
           <h2 className="text-[14px] text-500 letterSpacing-wider font-medium py-3 ml-4">
             PREVIEW
           </h2>
           <img
-            src={IconPreview}
-            alt="Preview Icon"
+            src={showPreviewOnly ? IconHidePreview : IconPreview}
+            alt="Preview Toggle Icon"
+            onClick={togglePreview}
             className="w-4 h-[11.2px] mr-4 hover:cursor-pointer"
           />
         </div>
