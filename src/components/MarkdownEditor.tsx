8c390fb8034e1c0c97b8a5ba6893001498c697a4
Sandeeb Adhikari <sa2734@njit.edu>
2024-11-15 21:27:06 -0500

    sidebar and functionlity

--- src/components/MarkdownEditor.tsx
+++ src/components/MarkdownEditor.tsx
@@ -11,10 +11,9 @@ const MarkdownEditor: React.FC = () => {
   };
 
   return (
-    <div className="flex  w-full min-h-screen bg-300">
-      {/* Markdown Editor */}
+    <div className="flex w-full min-h-screen bg-300">
       <div className="w-1/2 h-full bg-200">
-        <h2 className="text-[14px] text-500 letterSpacing-wider font-medium py-3 ml-4 ">
+        <h2 className="text-[14px] text-500 letterSpacing-wider font-medium py-3 ml-4">
           MARKDOWN
         </h2>
         <textarea
@@ -23,8 +22,7 @@ const MarkdownEditor: React.FC = () => {
           className="w-full h-screen p-4 bg-100 text-black hide-scrollbar focus:outline-none resize-none"
         />
       </div>
-      <div className=" flex w-[1px] h-full" />
-      {/* Markdown Preview */}
+      <div className="flex w-[1px] h-full" />
       <div className="w-1/2 flex flex-col items-center justify-center bg-200">
         <div className="flex items-center justify-between w-full">
           <h2 className="text-[14px] text-500 letterSpacing-wider font-medium py-3 ml-4">
@@ -36,7 +34,7 @@ const MarkdownEditor: React.FC = () => {
             className="w-4 h-[11.2px] mr-4 hover:cursor-pointer"
           />
         </div>
-        <div className="prose  max-w-full bg-100 p-4">
+        <div className="prose max-w-full bg-100 p-4">
           <ReactMarkdown>{markdownText}</ReactMarkdown>
         </div>
       </div>
---
+++ src/components/SideBar.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import IconDoc from "/assets/icon-document.svg";
+import IconClose from "/assets/icon-close.svg";
+
+interface SideBarProps {
+  isOpen: boolean;
+  toggleSidebar: () => void;
+}
+
+const SideBar: React.FC<SideBarProps> = ({ isOpen, toggleSidebar }) => {
+  return (
+    <>
+      <div
+        className={`fixed top-0 left-0 w-[250px] min-h-screen bg-900/95 transform text-500 ${
+          isOpen ? "translate-x-0" : "-translate-x-full"
+        } transition-transform duration-300 ease-in-out flex flex-col`}
+      >
+        <div className="flex items-center justify-between p-4">
+          <h1 className="text-[14px] font-light">MY DOCUMENTS</h1>
+          <button onClick={toggleSidebar} className="hover:cursor-pointer">
+            <img
+              src={IconClose}
+              alt="Close Icon"
+              className="w-[16px] h-[16px]"
+            />
+          </button>
+        </div>
+
+        <button className="mt-4 w-[202px] h-[40px] rounded bg-orange text-100 px-[44px] py-3 mx-6 text-[14px] flex items-center justify-start">
+          + New Document
+        </button>
+
+        <div className="mt-8 ml-6 flex flex-col gap-4">
+          <div className="flex items-center gap-2">
+            <img
+              src={IconDoc}
+              alt="Document Icon"
+              className="w-[14px] h-[16px]"
+            />
+            <div>
+              <p className="text-[12px] font-light text-gray-400">
+                01 April 2022
+              </p>
+              <p className="text-[13px] font-medium text-white">
+                untitled-document.md
+              </p>
+            </div>
+          </div>
+
+          <div className="flex items-center gap-2">
+            <img
+              src={IconDoc}
+              alt="Document Icon"
+              className="w-[14px] h-[16px]"
+            />
+            <div>
+              <p className="text-[12px] font-light text-gray-400">
+                01 April 2022
+              </p>
+              <p className="text-[13px] font-medium text-white">welcome.md</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SideBar;
